f2c88623f42752213f96b18aa04ebff7edb606a0
fisher <fisher@darling.dev>
2022-11-08 10:19:06 +0100

    Initial Commit

--- home.nix
+++ home.nix
@@ -8,11 +8,27 @@
     helix
     inetutils
     git
+    gnupg
   ];
   
-  home.stateVersion = "22.05";
+  home.stateVersion = "22.11";
   
   programs.home-manager.enable = true;
+  
+  programs.zsh = {
+    enable = true;
+    enableAutosuggestions = true;
+    enableCompletion = true;
+    
+    oh-my-zsh = {
+      enable = true;
+      plugins = [
+        "git"
+        "z"
+        "ssh-agent"
+      ];
+    };
+  };
   
   programs.git = {
     enable = true;
@@ -21,5 +37,14 @@
     extraConfig = {
       init.defaultBranch = "main";
     };
+  };
+  
+  services.gpg-agent = {
+    enable = true;
+    defaultCacheTtl = 1800;
+    enableSshSupport = true;
+    sshKeys = [
+      "/home/fisher/id_ed25519"
+    ];
   };
 }
