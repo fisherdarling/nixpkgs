123078eb4c640134fb2efbf04e495bd862307d30
Fisher Darling <fdarlingco@gmail.com>
2022-12-27 18:38:52 +0100

    works?

--- home.nix
+++ home.nix
@@ -4,7 +4,7 @@
   # Home Manager needs a bit of information about you and the
   # paths it should manage.
   home.username = "fisher";
-  home.homeDirectory = "/Users/fisher";
+  # home.homeDirectory = "/Users/fisher";
 
   # This value determines the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
