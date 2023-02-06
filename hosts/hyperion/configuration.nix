d3f99f5dd0fcc8ab87d8c53a2ba8fc06b6ad10aa
fisher <fisher@darling.dev>
2023-02-06 13:45:41 +0100

    add wine to hyperion

--- hosts/hyperion/configuration.nix
+++ hosts/hyperion/configuration.nix
@@ -128,6 +128,7 @@
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     curl
+    wine
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
