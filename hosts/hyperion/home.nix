ed4fff281f2cdb26fb6a9484e018f4e85d2f8a79
Fisher Darling <fdarlingco@gmail.com>
2023-06-01 22:40:27 +0200

    Per host config!

--- hosts/hyperion/home.nix
+++ hosts/hyperion/home.nix
@@ -1,4 +1,66 @@
-{ ... }:
+{ pkgs, lib, ... }:
 {
   imports = [ ../../home-manager/common.nix ];
+
+  programs.vscode = {
+    enable = true;
+    extensions = with pkgs.vscode-extensions; [
+      github.copilot
+      matklad.rust-analyzer
+      bungcip.better-toml
+      serayuzgur.crates
+    ];
+  };
+
+    programs.i3status-rust = {
+    enable = true;
+  };
+
+  xsession.windowManager.i3 = {
+    enable = true;
+
+    config = rec {
+      modifier = "Mod1";
+      terminal = "wezterm";
+
+      gaps.inner = 10;
+      gaps.outer = 10;      
+      
+      window.titlebar = false;
+
+      fonts = {
+        size = 32.0;
+      };
+
+      menu = "${pkgs.rofi}/bin/rofi";
+
+      bars = [
+        {
+          position = "bottom";
+          statusCommand = "${pkgs.i3status-rust}/bin/i3status-rs ~/.config/i3status-rust/config-default.toml";
+          fonts = {
+            size = 16.0;
+          };
+        }
+      ];
+
+      keybindings = lib.mkOptionDefault {
+       "${modifier}+d" = "exec --no-startup-id rofi -show drun";
+      };
+    };
+  };
+
+  programs.rofi = {
+    enable = true;
+    terminal = "${pkgs.wezterm}/bin/wezterm";
+  };
+
+  services.gpg-agent = {
+    enable = true;
+    defaultCacheTtl = 1800;
+    enableSshSupport = true;
+    sshKeys = [
+      "/Users/fisher/.ssh/id_ed25519"
+    ];
+  };
 }
--- hosts/lykos/configuration.nix
+++ hosts/lykos/configuration.nix
@@ -1,10 +1,12 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 
 {
   nix.settings = {
     keep-outputs = true;
     keep-derivations = true;
   };
+
+  nixpkgs.config.allowUnfree = true;
 
   # List packages installed in system profile. To search by name, run:
   # $ nix-env -qaP | grep wget
@@ -15,6 +17,8 @@
   # Use a custom configuration.nix location.
   # $ darwin-rebuild switch -I darwin-config=$HOME/.config/nixpkgs/darwin/configuration.nix
   # environment.darwinConfig = "$HOME/.config/nixpkgs/darwin/configuration.nix";
+
+  users.users.fisher.home = "/Users/fisher";
 
   # Auto upgrade nix package and the daemon service.
   services.nix-daemon.enable = true;
